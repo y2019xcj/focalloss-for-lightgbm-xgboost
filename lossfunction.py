f6ea966cde23dc3066ce3531a43342fa9b8b3858
y2019xcj <55391817+y2019xcj@users.noreply.github.com>
2021-04-18 15:09:44 +0800

    Update lossfunction.py

--- lossfunction.py
+++ lossfunction.py
@@ -9,15 +9,13 @@ class ComplexLoss:
     It will support xgboost and binary loss function later 
     '''
 
-    def __init__(self,alpha = 0.75,gamma = 0.5,smoothing_value = 0.1,num_class = 3,tree_type = 'lgb'):
+    def __init__(self,gamma = 0.5,num_class = 3,tree_type = 'lgb'):
         if alpha <= 0 or alpha >=1:
             raise Exception("alpha must in (0,1) ")
         self.alpha = alpha
         if gamma < 0:
             raise Exception('gamma must > 0')
         self.gamma = gamma
-        if smoothing_value < 0 or smoothing_value >= 1:
-            raise Exception('smoothing_value should be in [0,1)')
         self.smoothing_value = smoothing_value
         if tree_type != 'lgb':
             raise Exception('only support lightgbm now')
@@ -27,22 +25,22 @@ class ComplexLoss:
         self.num_class = num_class
 
     def focal_loss(self,y_true,y_pred):
+
+ 
         grad = np.zeros((len(y_true), self.num_class), dtype=float)
         hess = np.zeros((len(y_true), self.num_class), dtype=float)
 
         target = np.eye(self.num_class)[y_true.astype('int')]
-        pred = np.reshape(y_pred, (len(target), self.num_class), order='F')
-        '''label smoothing'''
-        target = (1 - self.smoothing_value) * target + self.smoothing_value / self.num_class 
-        eps = 1e-6
-        """get softmax probability"""
-        pred = np.exp(pred)
-        softmax_p = np.multiply(pred, 1/np.sum(pred, axis=1)[:, np.newaxis])
-        """get focal loss"""
-        """The alpha is no use while num_class > 2"""
-        softmax_p =  ((1 - softmax_p) ** self.gamma) * softmax_p
-        assert target.any() >= 0 or target.any() <= self.num_class
-        grad = (softmax_p - target)
-        hess = 2.0 * softmax_p * (1.0-softmax_p) + eps
-        hess[hess <= eps] = eps
+        pred = np.reshape(y_pred, (len(y_true), self.num_class), order='F')
+        # """get softmax probability"""
+        softmax_p = np.exp(pred)
+        softmax_p = np.multiply(softmax_p, 1/np.sum(softmax_p, axis=1)[:, np.newaxis])
+        for c in range(pred.shape[1]):
+            pc = softmax_p[:,c]
+            pt = softmax_p[:][target == 1]
+            grad[:,c][target[:,c] == 1] = (self.gamma * np.power(1-pt[target[:,c] == 1],self.gamma-1) * pt[target[:,c] == 1] * np.log(pt[target[:,c] == 1]) - np.power(1-pt[target[:,c] == 1],self.gamma) ) * (1 - pc[target[:,c] == 1])
+            grad[:,c][target[:,c] == 0] = (self.gamma * np.power(1-pt[target[:,c] == 0],self.gamma-1) * pt[target[:,c] == 0] * np.log(pt[target[:,c] == 0]) - np.power(1-pt[target[:,c] == 0],self.gamma) ) * (0 - pc[target[:,c] == 0])
+            hess[:,c][target[:,c] == 1] = (-4*(1-pt[target[:,c] == 1])*pt[target[:,c] == 1]*np.log(pt[target[:,c] == 1])+np.power(1-pt[target[:,c] == 1],2)*(2*np.log(pt[target[:,c] == 1])+5))*pt[target[:,c] == 1]*(1-pt[target[:,c] == 1])
+            hess[:,c][target[:,c] == 0] = pt[target[:,c] == 0]*np.power(pc[target[:,c] == 0],2)*(-2*pt[target[:,c] == 0]*np.log(pt[target[:,c] == 0])+2*(1-pt[target[:,c] == 0])*np.log(pt[target[:,c] == 0]) + 4*(1-pt[target[:,c] == 0])) - pc[target[:,c] == 0]*(1-pc[target[:,c] == 0])*(1-pt[target[:,c] == 0])*(2*pt[target[:,c] == 0]*np.log(pt[target[:,c] == 0]) - (1-pt[target[:,c] == 0]))
+
         return grad.flatten('F'), hess.flatten('F')
